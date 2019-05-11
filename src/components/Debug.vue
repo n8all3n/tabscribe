fde1ea363640ec7a5c6bddc1e3ed306c2105e5a4
n8 <n8all3n@gmail.com>
2019-05-10 20:31:15 -0600

    Functionality to add multiple lines and style fixes

---
+++ src/components/Debug.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+    <!-- debug -->
+
+    </div>
+</template>
+<script>
+export default {
+    /* eslint-disable */
+  name: 'HelloWorld',
+   props: [
+    "item"
+  ],
+  created: function() {
+    console.log(this.item);
+  }
+}
+</script>
--- src/components/Tabscribe.vue
+++ src/components/Tabscribe.vue
@@ -1,54 +1,74 @@
 <template>
-    <div>
-        <div class="tab-container">
-            <div class="string" v-for="(item, stringIndex) in stringCount" v-bind:key="stringIndex">
-                <div class="string-tuning">
-                    {{stringTuning[stringIndex]}}
-                </div>
-                <div class="adv-container">
-                   <div class="btn-group">
-                        <button class="btn btn-secondary btn-sm dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            {{getSpecialNotationVal(stringIndex)}}
-                        </button>
-                        <div class="dropdown-menu">
-                            <a class="dropdown-item" href="javascript:void(0);" @click="setSpecialNotationValue('-', stringIndex);">-</a>
-                            <a class="dropdown-item" href="javascript:void(0);" @click="setSpecialNotationValue('h', stringIndex);">h</a>
-                            <a class="dropdown-item" href="javascript:void(0);" @click="setSpecialNotationValue('p', stringIndex);">p</a>
-                            <a class="dropdown-item" href="javascript:void(0);" @click="setSpecialNotationValue('b', stringIndex);">b</a>
-                            <a class="dropdown-item" href="javascript:void(0);" @click="setSpecialNotationValue('r', stringIndex);">r</a>
-                            <a class="dropdown-item" href="javascript:void(0);" @click="setSpecialNotationValue('/', stringIndex);">/</a>
-                            <a class="dropdown-item" href="javascript:void(0);" @click="setSpecialNotationValue('\\', stringIndex);">\</a>
-                            <a class="dropdown-item" href="javascript:void(0);" @click="setSpecialNotationValue('v', stringIndex);">v</a>
-                            <a class="dropdown-item" href="javascript:void(0);" @click="setSpecialNotationValue('t', stringIndex);">t</a>
-                            <a class="dropdown-item" href="javascript:void(0);" @click="setSpecialNotationValue('x', stringIndex);">x</a>                  
+    <div class="tabscribe-container">
+        <div class="sticky-top">
+            <div class="tab-container">
+                <div class="string" v-for="(item, stringIndex) in stringCount" v-bind:key="stringIndex">
+                    <div class="string-tuning">
+                        {{stringTuning[stringIndex]}}
+                    </div>
+                    <div class="adv-container">
+                    <div class="btn-group">
+                            <button class="btn btn-secondary btn-sm dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                {{getSpecialNotationVal(stringIndex)}}
+                            </button>
+                            <div class="dropdown-menu">
+                                <a class="dropdown-item" href="javascript:void(0);" @click="setSpecialNotationValue('-', stringIndex);">-</a>
+                                <a class="dropdown-item" href="javascript:void(0);" @click="setSpecialNotationValue('h', stringIndex);">h</a>
+                                <a class="dropdown-item" href="javascript:void(0);" @click="setSpecialNotationValue('p', stringIndex);">p</a>
+                                <a class="dropdown-item" href="javascript:void(0);" @click="setSpecialNotationValue('b', stringIndex);">b</a>
+                                <a class="dropdown-item" href="javascript:void(0);" @click="setSpecialNotationValue('r', stringIndex);">r</a>
+                                <a class="dropdown-item" href="javascript:void(0);" @click="setSpecialNotationValue('/', stringIndex);">/</a>
+                                <a class="dropdown-item" href="javascript:void(0);" @click="setSpecialNotationValue('\\', stringIndex);">\</a>
+                                <a class="dropdown-item" href="javascript:void(0);" @click="setSpecialNotationValue('v', stringIndex);">v</a>
+                                <a class="dropdown-item" href="javascript:void(0);" @click="setSpecialNotationValue('t', stringIndex);">t</a>
+                                <a class="dropdown-item" href="javascript:void(0);" @click="setSpecialNotationValue('x', stringIndex);">x</a>                  
+                            </div>
                         </div>
                     </div>
+                    <div class="fret" v-bind:class="getFretNoteClass(fretIndex, stringIndex)" v-for="(item,fretIndex) in fretCount" v-bind:key="fretIndex" v-on:click="fretClick(fretIndex + 1, stringIndex);">
+                    </div>
                 </div>
-                <div class="fret" v-bind:class="getFretNoteClass(fretIndex, stringIndex)" v-for="(item,fretIndex) in fretCount" v-bind:key="fretIndex" v-on:click="fretClick(fretIndex + 1, stringIndex);">
-                </div>
+            </div>
+            <div class="button-group">
+                <button type="button" v-on:click="addNewLine();">Add Line</button>
+                <button type="button" v-on:click="deleteLine();">Delete Line</button>
+                <button type="button" v-on:click="addNewBar();">Add Bar</button>
+                <button type="button" v-on:click="backOneBar();">Back a Bar</button>
+                <button type="button" v-on:click="nextBar();">Next Bar</button>
+                <button type="button" v-on:click="deleteBar();">Delete Bar</button>
             </div>
         </div>
-        <span>Active bar is {{activeBar}}</span>
-        <button type="button" v-on:click="addNewBar();">Add Bar</button>
-        <button type="button" v-on:click="backOneBar();">Back a Bar</button>
-        <button type="button" v-on:click="nextBar();">Next Bar</button>
-        <button type="button" v-on:click="deleteBar();">Delete Bar</button>
-        <div class="notation-container">
-            <div class="notation-string" v-for="(item, stringIndex) in stringCount" v-bind:key="stringIndex">
-                <div class="notation-string-tuning"> 
-                    <pre class="notation-text">{{stringTuning[stringIndex]}}|</pre>
+         
+
+        <div class="notation-groups">
+            <div class="notation-container" v-for="(currentLine, lineIndex) in lines.length" v-bind:key="lineIndex">
+                <div class="notation-string" v-for="(string, stringIndex) in stringCount" v-bind:key="stringIndex">
+                    <div class="notation-string-tuning"> 
+                        <pre class="notation-text">{{stringTuning[stringIndex]}}|</pre>
+                    </div>
+
+                    <div class="notation-bar" v-bind:class="activeLine === lineIndex && activeBar === barIndex ? 'active-bar' : ''"
+                        v-for="(item,barIndex) in lines[lineIndex].length" :key="barIndex"
+                        v-on:click="setBarActive(lineIndex, barIndex);" 
+                        v-on:mouseover="barMouseOver(barIndex);" 
+                        v-on:mouseleave="barMouseLeave();">
+
+                        <debug :item="lines[lineIndex][barIndex][stringIndex]"/>
+                        <span class="notation-text"
+                            v-bind:class="[hoverBar === barIndex ? 'hovered-bar' : '']">
+                            {{lines[lineIndex][barIndex][stringIndex]}}</span>
+                    </div>
                 </div>
-                <div class="notation-bar" v-on:click="setBarActive(barIndex);" v-on:mouseover="barMouseOver(barIndex);"  v-on:mouseleave="barMouseLeave();" v-for="(item,barIndex) in barText.length" :key="barIndex" v-bind:data-string="stringIndex + 1" v-bind:data-bar="barIndex + 1">
-                    <pre class="notation-text" v-bind:class="[hoverBar === barIndex ? 'hovered-bar' : '', activeBar === barIndex ? 'active-bar' : '']">{{barText[barIndex][stringIndex]}}</pre>
-                </div>
-            </div>
-        </div> 
+            </div> 
+        </div>
+
     </div>
 </template>
 
 <script>
 /* eslint-disable */
 import Vue from 'vue'
+import debug from '../components/Debug.vue'
 export default {
   name: 'Tabscribe',
   props: {
@@ -61,34 +81,64 @@ export default {
           stringTuning: [],
           hoverBar: null,
           activeBar: 0,
+          activeLine: 0,
+          lines:[],
           barText: []
       }
   },
+  components: {
+    debug
+  },
   created: function () {
-      // Set default strings and tunings
-      if (this.stringCount === 7) {
-        this.stringTuning = ["E", "B", "G", "D", "A", "E", "B"];
-      } else if (this.stringCount == 6) {
-        this.stringTuning = ["E", "A", "D", "G", "B", "E"];
-      } else if (this.stringCount === 4) {
-          this.stringTuning = ["G", "D", "A", "E"];
-      }
+    // Set default strings and tunings
+    if (this.stringCount === 7) {
+    this.stringTuning = ["E", "B", "G", "D", "A", "E", "B"];
+    } else if (this.stringCount == 6) {
+    this.stringTuning = ["E", "A", "D", "G", "B", "E"];
+    } else if (this.stringCount === 4) {
+        this.stringTuning = ["G", "D", "A", "E"];
+    }
 
-      // default some text into the bars
-      this.barText[0] = ['-','-','-','-'];
+    this.lines[0] = [];
+    this.lines[0].push(['-','-','-','-']);
+
+
+    //this.lines[0][0] = ['-','-','-','-'];
+
+    // default some text into the bars
+    this.barText[0] = ['-','-','-','-'];
 
   },
   methods: {
-    setSpecialNotationValue(notation, stringIndex) {
-        // find the current bar
-        var currentBarText = this.barText[this.activeBar];
+    addNewLine: function() {
+        var newBar = ['-','-','-','-'];
+        
+        var newArray = [];
+        newArray[0] = newBar;
+        
+        var nextIndex = this.lines.length;
 
-        currentBarText[stringIndex] = notation;
-        Vue.set(this.barText, this.activeBar, currentBarText);
+        Vue.set(this.lines, nextIndex, newArray);
+    },
+    deleteLine: function() {
+        if (this.lines.length > 1) {
+            this.$delete(this.lines, this.activeLine);
+
+            if (this.activeLine > 0) {
+                this.activeLine--;
+            }
+
+            this.activeBar = 0;
+        }
+    },
+    setSpecialNotationValue(notation, stringIndex) {
+        var currLine = this.lines[this.activeLine];
+        currLine[this.activeBar][stringIndex] = notation;
+        Vue.set(this.lines, this.activeLine, currLine);
 
     },
     getSpecialNotationVal(stringIndex) {
-        var currBar = this.barText[this.activeBar];
+        var currBar = this.lines[this.activeLine][this.activeBar];
         var theString = currBar[stringIndex];
 
         var parsedVal = parseInt(theString, 10);
@@ -104,7 +154,7 @@ export default {
        * return a class that indicates the fret is active
        */
       getFretNoteClass: function(fretIndex, stringIndex) {
-        var currBar = this.barText[this.activeBar];
+        var currBar =  this.lines[this.activeLine][this.activeBar];
         var theString = currBar[stringIndex];
 
         if (parseInt(theString, 10) - 1 === fretIndex) {
@@ -114,22 +164,33 @@ export default {
         return '';
       },
       fretClick: function(fretIndex, stringIndex) {
-        // find the current bar
-        var currentBarText = this.barText[this.activeBar];
+        var currLine = this.lines[this.activeLine];
 
-        // clicking on an item that is already selected...clear the item
-        if (currentBarText[stringIndex] === fretIndex.toString()) {
-            currentBarText[stringIndex] = '-';
+        if (currLine[this.activeBar][stringIndex] === fretIndex) {
+            currLine[this.activeBar][stringIndex] = '-';
         } else {
-            // clicked on a new fret set the fret
-            currentBarText[stringIndex] = fretIndex.toString(); 
+            currLine[this.activeBar][stringIndex] = fretIndex;
         }
 
-        Vue.set(this.barText, this.activeBar, currentBarText);
+        Vue.set(this.lines, this.activeLine, currLine);      
+
+
+        // // find the current bar
+        // var currentBarText = this.barText[this.activeBar];
+
+        // // clicking on an item that is already selected...clear the item
+        // if (currentBarText[stringIndex] === fretIndex.toString()) {
+        //     currentBarText[stringIndex] = '-';
+        // } else {
+        //     // clicked on a new fret set the fret
+        //     currentBarText[stringIndex] = fretIndex.toString(); 
+        // }
+
+        // Vue.set(this.barText, this.activeBar, currentBarText);
       },
       deleteBar: function () {
         // don't delete if there's just one bar
-        if (this.barText.length === 1) {
+        if (this.lines[this.activeLine].length === 1) {
             return;
         }
           
@@ -141,8 +202,8 @@ export default {
             nextActiveBar = this.activeBar - 1;
         }
         
-        this.$delete(this.barText, this.activeBar);
-
+        this.$delete(this.lines[this.activeLine], this.activeBar);
+        
         // update the active bar now that a bar is deleted
         this.activeBar = nextActiveBar;
       },
@@ -153,10 +214,18 @@ export default {
             newBar.push('-');
         }
 
-        Vue.set(this.barText, this.barText.length, newBar);
+        //this.lines[this.activeLine].push(newBar);
+
+        var nextish = this.lines[this.activeLine];
+        nextish.push(newBar);
+
+        Vue.set(this.lines, this.activeLine, nextish);
+
+
+        //Vue.set(this.barText, this.barText.length, newBar);
       },
       nextBar: function() {
-        if (this.activeBar + 1 >= this.barText.length) {
+        if (this.activeBar + 1 >= this.lines[this.activeLine].length) {
             return;
         }
 
@@ -176,8 +245,9 @@ export default {
       barMouseLeave: function() {
         this.hoverBar = null;
       },
-      setBarActive: function(index) {
-        this.activeBar = index;
+      setBarActive: function(lineIndex, barIndex) {
+        this.activeBar = barIndex;
+        this.activeLine = lineIndex;
       }
   }
 }
@@ -185,8 +255,19 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+    /* .button-group {
+        display: flex;
+        flex-direction: row;
+    } */
+
+    /* .tabscribe-container {
+        display: flex;
+        flex-direction: column;
+    } */
+
     .adv-container {
-        padding: .2rem;
+        padding: .1em;
     }
     .fret-active {
         background:lightseagreen;
@@ -197,7 +278,8 @@ export default {
         width: 100%;
         border-top: 2px solid #4E5555;
         border-bottom: 2px solid #4E5555;
-        border-collapse: collapse
+        border-collapse: collapse;
+        background-color: white;
     }
 
     .string {
@@ -216,10 +298,15 @@ export default {
         background-color: red;
     }
 
-
     .notation-container {
         display: table;
+        margin-bottom: 1rem;
     }
+
+    /* .notation-groups {
+        height: 15em;
+        overflow: auto;
+    } */
 
     .notation-string {
         display: table-row;
